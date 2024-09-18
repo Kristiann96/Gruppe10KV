c25de8673d4dbeac9b2801ae8c0426da5f48c39e
Kristiann96 <kristianni@uia.no>
2024-09-18 12:12:42 +0200

    Remove extra comment //

--- Gruppe10KVprototype/wwwroot/js/MapAPI.js
+++ Gruppe10KVprototype/wwwroot/js/MapAPI.js
@@ -2,7 +2,7 @@
 var map = L.map('map').setView([65.5, 17.0], 4);
 
 
-//// Add a tile layer (OpenStreetMap)
+// Add a tile layer (OpenStreetMap)
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
