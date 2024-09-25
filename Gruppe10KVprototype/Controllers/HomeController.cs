7f7c54a82b40ee3f37d6533f5829fd466c7a50f4
Martinstomnas <144673018+Martinstomnas@users.noreply.github.com>
2024-09-25 13:38:47 +0200

    Skjema - draft frontend

--- Gruppe10KVprototype/Controllers/HomeController.cs
+++ Gruppe10KVprototype/Controllers/HomeController.cs
@@ -27,7 +27,10 @@ namespace Gruppe10KVprototype.Controllers
         {
             return View();
         }
-
+        public IActionResult Form()
+         {
+        return View();
+        }
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
