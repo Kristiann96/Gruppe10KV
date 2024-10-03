2a280f0f4a6850b8e363b837f3d670d6ac5af9f3
Galescape <orjofid@gmail.com>
2024-10-03 14:26:04 +0200

    Legger til henting av data

--- Gruppe10KVprototype/Controllers/HomeController.cs
+++ Gruppe10KVprototype/Controllers/HomeController.cs
@@ -8,7 +8,7 @@ namespace Gruppe10KVprototype.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
-
+        
         public HomeController(ILogger<HomeController> logger)
         {
             _logger = logger;
