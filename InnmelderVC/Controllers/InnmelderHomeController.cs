4c3a3ddede6deb1a4b431f432b5a1413f6664413
Kristiann96 <kristianni@uia.no>
2024-10-04 14:17:57 +0200

    Refaktorering til korrrekte moduler og mappestrukturer. Lagt til index view i Innmelder modul. Opprettet prosjektreferanser i henhold til planlagt arkitektur.

---
+++ InnmelderVC/Controllers/InnmelderHomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace InnmelderVC.Controllers
+{
+    public class InnmelderHomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
