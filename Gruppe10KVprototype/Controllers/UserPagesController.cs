afd1d3d4c2fdfd5db1f32d548652c8dfa5a38a77
Daniel <mogsterdaniel@gmail.com>
2024-10-09 14:58:04 +0200

    Made a new "user cases" page
    Added a "User Cases" page and some demo cases.

---
+++ Gruppe10KVprototype/Controllers/UserPagesController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Gruppe10KVprototype.Controllers
+{
+    public class UserPagesController : Controller
+    {
+        public IActionResult UserCases()
+        {
+            return View();
+        }
+    }
+}
