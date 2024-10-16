7a41c54b9265574f9e8fb971602a6c7bdce0e461
Lene Olsen <leneo12@student.uia.no>
2024-10-16 05:45:02 +0200

    Fjernet duplisert kode

--- Gruppe10KVprototype/Program.cs
+++ Gruppe10KVprototype/Program.cs
@@ -8,20 +8,14 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
 builder.Services.AddRazorPages();
 
-// Register DBContext as a service
+// Register DapperDBConnection as a service
 builder.Services.AddScoped<DapperDBConnection>();
-builder.Services.AddScoped<IInnmelderRepository, InnmelderRepository>();
 
-builder.Services.AddScoped<DapperDBConnection>();
+//Registrering av repos og interfaces
+builder.Services.AddScoped<IInnmelderRepository, InnmelderRepository>();
 builder.Services.AddScoped<ISaksbehandlerRepository, SaksbehandlerRepository>();
 
-// Adding ApplicationDbContext as a service to allow our application to use CaseService for database operations and retrieving user cases.
-/*builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseMySql(builder.Configuration.GetConnectionString("MariaDbConnection"),
-    new MySqlServerVersion(new Version(11, 5, 2))));
 
-// Register CaseService
-builder.Services.AddScoped<CaseService>();*/
 
 
 var app = builder.Build();
