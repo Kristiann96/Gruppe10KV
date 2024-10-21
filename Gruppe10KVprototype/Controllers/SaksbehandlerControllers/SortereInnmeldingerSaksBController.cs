ac4159f73ef20ccf8ab38eae8b0853bba59df790
Lene Olsen <leneo12@student.uia.no>
2024-10-21 18:10:06 +0200

    Alle navn endret. Alt virker! HuzzaH

---
+++ Gruppe10KVprototype/Controllers/SaksbehandlerControllers/SortereInnmeldingerSaksBController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Gruppe10KVprototype.Controllers.SaksbehandlerControllers
+{
+    public class SortereInnmeldingerSaksBController : Controller
+    {
+        public IActionResult SortereInnmeldingerSaksB()
+        {
+            return View();
+        }
+    }
+}
