634f083dedfac121d2f9a321e61dcb93183cd543
Lene Olsen <leneo12@student.uia.no>
2024-10-27 22:57:38 +0100

    Seperation of concern - skiller config, services og utils for kart api'er i /wwwroot/js/maps Oppdaterer views med visning av kart til å bruke disse .js filer

---
+++ Gruppe10KVprototype/wwwroot/js/maps/config/drawConfig.js
@@ -0,0 +1,48 @@
+﻿// drawConfig.js
+const DrawConfig = {
+    // Konfigurasjon for tegne-verktøy
+    drawOptions: {
+        draw: {
+            polygon: true,
+            polyline: true,
+            rectangle: true,
+            circle: true,
+            marker: true
+        },
+        edit: {
+            featureGroup: null, // Dette settes når vi initialiserer
+            remove: true
+        }
+    },
+
+    // Initialiser tegne-verktøy på et kart
+    initializeDrawControl: function (map) {
+        // Opprett en FeatureGroup for å lagre tegnede elementer
+        const drawnItems = new L.FeatureGroup();
+        map.addLayer(drawnItems);
+
+        // Oppdater edit options med FeatureGroup
+        this.drawOptions.edit.featureGroup = drawnItems;
+
+        // Opprett og legg til tegne-kontroll
+        const drawControl = new L.Control.Draw(this.drawOptions);
+        map.addControl(drawControl);
+
+        // Håndter tegne-hendelser
+        map.on(L.Draw.Event.CREATED, function (event) {
+            const layer = event.layer;
+            drawnItems.addLayer(layer);
+
+            // Konverter til GeoJSON
+            const geoJsonData = layer.toGeoJSON();
+
+            // Hvis det finnes et skjult felt, oppdater det
+            const hiddenField = document.getElementById('geoJsonHidden');
+            if (hiddenField) {
+                hiddenField.value = JSON.stringify(geoJsonData);
+            }
+        });
+
+        return drawnItems;
+    }
+};
