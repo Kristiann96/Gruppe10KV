36699952208dcf7ad70253fb3224b8e75a78d8b6
Lene Olsen <leneo12@student.uia.no>
2024-11-01 17:33:46 +0100

    Lager filer klare for  refactorering av mapview og innmelding som går via incident_form Har lagt til dataflyt .mmd

---
+++ Gruppe10KVprototype/Controllers/InnmelderControllers/KartfeilSkjemaBekreftelseController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Gruppe10KVprototype.Controllers.InnmelderControllers
+{
+    public class KartfeilSkjemaBekreftelseController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
+//her kommer refactorert InnmelderSkjemaIncidentFormController - denne er pr idag en kontroller for 2 views
