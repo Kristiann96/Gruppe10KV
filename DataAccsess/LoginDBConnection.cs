9f143d1c1ff3dc71698fce2d54432b3b64136b96
Kristiann96 <kristianni@uia.no>
2024-11-05 10:56:23 +0100

    lagt til data access til ny server

---
+++ DataAccsess/LoginDBConnection.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.Configuration;
+using MySqlConnector;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess
+{
+    internal class LoginDbConnection
+    {
+        private readonly string _connectionString;
+
+        public LoginDbConnection(IConfiguration configuration)
+        {
+            _connectionString = configuration.GetConnectionString("MariaDbConnection_login_server")!;
+        }
+
+        public MySqlConnection CreateConnection()
+        {
+            var connection = new MySqlConnection(_connectionString);
+            connection.Open();
+            return connection;
+        }
+    }
+}
