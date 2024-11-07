c2a9bf793cbb0e85a12c051fa57cd450673efa00
Kristiann96 <kristianni@uia.no>
2024-11-07 11:21:05 +0100

    controller metode for mottak av registreringsdata. inneholder IdentityUser objekt og authentication

--- Gruppe10KVprototype/Controllers/InnmelderControllers/RegistrerDegController.cs
+++ Gruppe10KVprototype/Controllers/InnmelderControllers/RegistrerDegController.cs
@@ -1,21 +1,51 @@
 ﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using ViewModels;
 
-namespace Gruppe10KVprototype.Controllers.InnmelderControllers
+namespace Gruppe10KVprototype.Controllers.InnmelderControllers;
+public class RegistrerDegController : Controller
 {
-    public class RegistrerDegController : Controller
-    {
-        public IActionResult RegistrerDeg()
-        {
-            return View();
-        }
+    private readonly UserManager<IdentityUser> _userManager;
 
-        [HttpGet]
-        [AllowAnonymous]
-        public IActionResult RegistrerInnmelder(string returnUrl = null)
+    public RegistrerDegController(UserManager<IdentityUser> userManager)
+    {
+        _userManager = userManager;
+    }
+
+    public IActionResult RegistrerDeg()
+    {
+        return View();
+    }
+
+    [HttpGet]
+    [AllowAnonymous]
+    public IActionResult RegistrerInnmelder(string returnUrl = null)
+    {
+        ViewData["ReturnUrl"] = returnUrl;
+        return View("RegistrerDeg");
+    }
+
+
+    [HttpPost]
+    [AllowAnonymous]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> RegistrerInnmelder(BrukerregistreringViewModel model, string returnUrl = null)
+    {
+        ViewData["ReturnUrl"] = returnUrl;
+        var user = new IdentityUser { UserName = model.Email, Email = model.Email };
+        var result = await _userManager.CreateAsync(user, model.Password);
+        return RedirectToLocal(returnUrl);
+    }
+    public IActionResult RedirectToLocal(string returnUrl)
+    {
+        if (Url.IsLocalUrl(returnUrl))
         {
-            ViewData["ReturnUrl"] = returnUrl;
-            return View("RegistrerDeg");
+            return Redirect(returnUrl);
+        }
+        else
+        {
+            return RedirectToAction(nameof(RegistrerDeg));
         }
     }
 }
