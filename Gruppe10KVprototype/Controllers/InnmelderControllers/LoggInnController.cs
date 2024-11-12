65865b8e997205d9ea68d625be9d558d3ed8afd7
Kristiann96 <kristianni@uia.no>
2024-11-12 22:08:02 +0100

    Fungerende logg inn implementert

--- Gruppe10KVprototype/Controllers/InnmelderControllers/LoggInnController.cs
+++ Gruppe10KVprototype/Controllers/InnmelderControllers/LoggInnController.cs
@@ -1,12 +1,53 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Authorization;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Mvc;
+using ViewModels;
 
-namespace Gruppe10KVprototype.Controllers.InnmelderControllers
+
+
+namespace Gruppe10KVprototype.Controllers.InnmelderControllers;
+public class LoggInnController : Controller
+
 {
-    public class LoggInnController : Controller
+    private readonly SignInManager<IdentityUser> _signInManager;
+    private readonly ILogger<LoggInnController> _logger;
+    public LoggInnController(SignInManager<IdentityUser> signInManager, ILoggerFactory loggerFactory)
     {
-        public IActionResult LoggInn()
+        _signInManager = signInManager;
+        _logger = loggerFactory.CreateLogger<LoggInnController>();
+    }
+
+    [HttpPost]
+    [AllowAnonymous]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> LoggInn(LoggInnViewModel model)
+    {
+        if (!ModelState.IsValid)
         {
             return View();
         }
+        
+        var result = await _signInManager.PasswordSignInAsync(model.Email, model.Password, false, lockoutOnFailure: false);
+        if (result.Succeeded)
+        {
+            _logger.LogInformation(1, "User logged in.");
+            return RedirectToAction("LandingsSide", "LandingsSide");
+        }
+        else
+        {
+            ModelState.AddModelError(string.Empty, "Feil brukernavn eller passord");
+            return View("LoggInn");
+        }
+    }
+    
+    public IActionResult MidlertidigBypass()
+    {
+        return RedirectToAction("LandingsSide", "LandingsSide");
+    }
+    public IActionResult VisLoggInnSide()
+    {
+        return View("LoggInn");
     }
 }
+
