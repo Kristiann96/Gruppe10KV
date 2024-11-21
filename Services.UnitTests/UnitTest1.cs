d4df1fab9dbf144e807292ac3c584823cab97fbd
Galescape <orjofid@gmail.com>
2024-11-21 11:23:15 +0100

    Prøver å finpusse testen. Testen "Sikrer at OppdatereInnmeldingAsync ikke kaller repository når validering feiler". Legger til kommentarer for å fortelle gruppa hva delene gjør. Mer robust for refaktorering

--- Services.UnitTests/UnitTest1.cs
+++ Services.UnitTests/UnitTest1.cs
@@ -2,52 +2,70 @@ using Interface;
 using LogicInterfaces;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Models.Entities;
-using Models.Exceptions;
 using Moq;
 using ViewModels;
+using Models.Exceptions;
 
-namespace Services.UnitTests;
-
-[TestClass]
-public class OppdatereInnmeldingServiceTests
+namespace Services.UnitTests
 {
-    private Mock<IInnmeldingRepository> _mockInnmeldingRepo = null!;
-    private Mock<IInnmeldingLogic> _mockInnmeldingLogic = null!;
-    private OppdatereInnmeldingService _service = null!;
-
-    [TestInitialize]
-    public void TestInitialize()
+    [TestClass]
+    public class OppdatereInnmeldingServiceTests
     {
-        _mockInnmeldingRepo = new Mock<IInnmeldingRepository>();
-        _mockInnmeldingLogic = new Mock<IInnmeldingLogic>();
-        _service = new OppdatereInnmeldingService(
-            _mockInnmeldingRepo.Object,
-            null!,  // IGeometriRepository ikke brukt i denne testen
-            null!,  // ITransaksjonsRepository ikke brukt i denne testen
-            _mockInnmeldingLogic.Object
-        );
-    }
+        private Mock<IInnmeldingRepository> _mockInnmeldingRepo = null!;
+        private Mock<IInnmeldingLogic> _mockInnmeldingLogic = null!;
+        private OppdatereInnmeldingService _service = null!;
 
-    [TestMethod]
-    [Description("Verifiserer at riktig exception type kastes fra logikklaget og at repository ikke kalles")]
-    public async Task OppdatereInnmelding_NaarValideringFeilerILogikk_SkalIkkeKallePaaRepository()
-    {
-        // Arrange
-        var testViewModel = new OppdatereInnmeldingViewModel
+        [TestInitialize]
+        public void TestInitialize()
         {
-            InnmeldingId = 1,
-            Tittel = "Test",
-            Beskrivelse = "Test"
-        };
+            // Oppretter mock-objekter for å simulere avhengigheter
+            _mockInnmeldingRepo = new Mock<IInnmeldingRepository>();
+            _mockInnmeldingLogic = new Mock<IInnmeldingLogic>();
 
-        _mockInnmeldingLogic.Setup(x => x.ValiderInnmeldingData(It.IsAny<InnmeldingModel>()))
-            .ThrowsAsync(new ForretningsRegelExceptionModel("Data er ikke gyldig"));
+            // Oppretter service-instans med mock-avhengigheter
+            _service = new OppdatereInnmeldingService(
+                _mockInnmeldingRepo.Object,
+                null!, // Null for andre avhengigheter som ikke brukes i denne testen
+                null!,
+                _mockInnmeldingLogic.Object
+            );
+        }
 
-        // Act & Assert
-        await Assert.ThrowsExceptionAsync<ForretningsRegelExceptionModel>(() =>
-            _service.OppdatereInnmeldingAsync(testViewModel));
+        [TestMethod]
+        [Description("Sikrer at OppdatereInnmeldingAsync ikke kaller repository når validering feiler")]
+        public async Task OppdatereInnmeldingAsync_ValideringFeiler_SkalIkkeKalleRepository()
+        {
+            // Arrange - Oppretter testdata
+            var testViewModel = new OppdatereInnmeldingViewModel
+            {
+                InnmeldingId = 1,
+                Tittel = "Testtittel",
+                Beskrivelse = "Testbeskrivelse"
+            };
 
-        _mockInnmeldingRepo.Verify(r => r.OppdatereInnmeldingAsync(It.IsAny<InnmeldingModel>()), 
-            Times.Never());
+            // Simulerer at valideringslogikken kaster ForretningsRegelExceptionModel
+            _mockInnmeldingLogic
+                .Setup(x => x.ValiderInnmeldingData(It.IsAny<InnmeldingModel>()))
+                .ThrowsAsync(new ForretningsRegelExceptionModel("Validering feilet"));
+
+            // Act & Assert - Verifiserer at unntaket kastes
+            var exception = await Assert.ThrowsExceptionAsync<ForretningsRegelExceptionModel>(() => 
+                _service.OppdatereInnmeldingAsync(testViewModel));
+            
+            // Verifiserer feilmeldingen for ekstra trygghet
+            Assert.AreEqual("Validering feilet", exception.Message);
+
+            // Verifiserer at repository-metoden aldri blir kalt
+            _mockInnmeldingRepo.Verify(
+                x => x.OppdatereInnmeldingAsync(It.IsAny<InnmeldingModel>()), 
+                Times.Never()
+            );
+
+            // Ekstra verifisering for å sikre at det ikke er uforventede kall
+            _mockInnmeldingLogic.Verify(
+                x => x.ValiderInnmeldingData(It.IsAny<InnmeldingModel>()), 
+                Times.Once() // Bekrefter at validering kun ble kalt én gang
+            );
+        }
     }
 }
