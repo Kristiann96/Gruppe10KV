7136ff16f11841b93bff8f296dece13fc80923c9
Martin Stomnås <144673018+Martinstomnas@users.noreply.github.com>
2024-11-21 11:40:26 +0100

    Unit_Test_Logic

--- Logic.UnitTests/UnitTest1.cs
+++ Logic.UnitTests/UnitTest1.cs
@@ -1,10 +1,28 @@
-namespace Logic.UnitTests;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Models.Entities;
+using Models.Models;
+using Models.Exceptions;
+using Interface;
+using Logic;
 
 [TestClass]
-public class UnitTest1
+public class InnmeldingLogicTests
 {
-    [TestMethod]
-    public void TestMethod1()
+    private Mock<ITransaksjonsRepository> _transaksjonsRepositoryMock;
+    private Mock<IGeometriRepository> _geometriRepositoryMock;
+    private InnmeldingLogic _logic;
+
+    [TestInitialize]
+    public void Initialize()
     {
+        _transaksjonsRepositoryMock = new Mock<ITransaksjonsRepository>();
+        _geometriRepositoryMock = new Mock<IGeometriRepository>();
+        _logic = new InnmeldingLogic(_transaksjonsRepositoryMock.Object, _geometriRepositoryMock.Object);
     }
+
+    
 }
