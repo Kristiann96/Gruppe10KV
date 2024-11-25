479f5754bcf6da173ffaee58595f71cf104dfab3
Galescape <orjofid@gmail.com>
2024-11-25 10:45:35 +0100

    Fjerner kommentarer i ViewModels

--- ViewModels/BehandleInnmeldingSaksBViewModel.cs
+++ ViewModels/BehandleInnmeldingSaksBViewModel.cs
@@ -7,44 +7,29 @@ namespace ViewModels;
 
 public class BehandleInnmeldingSaksBViewModel
 {
-
     public int InnmeldingId { get; set; }
     public string Tittel { get; set; } = null!;
     public string Beskrivelse { get; set; } = null!;
-
     public string Status { get; set; }
     public string Prioritet { get; set; }
     public string KartType { get; set; }
-
-    // Person-detaljer for innmelderen
     public string Fornavn { get; set; } = null!;
     public string Etternavn { get; set; } = null!;
     public string Telefonnummer { get; set; } = null!;
-
-    // Innmelder-detaljer
     public int InnmelderId { get; set; }
     public string InnmelderType { get; set; } = null!;
-
-    // Saksbehandler-detaljer
     public string SaksbehandlerStilling { get; set; } = null!;
     public string SaksbehandlerJobbepost { get; set; } = null!;
     public string SaksbehandlerJobbtelefon { get; set; } = null!;
-
     public int GjestInnmelderId { get; set; } 
-
-
-
     public string GeometriGeoJson { get; set; }
     public List<SelectListItem> StatusOptions { get; set; }
     public List<SelectListItem> PrioritetOptions { get; set; }
     public List<SelectListItem> KartTypeOptions { get; set; }
-
-
     public int? ValgtSaksbehandlerId { get; set; }
     public List<SelectListItem> InnmelderTypeOptions { get; set; }
-
     public List<(SaksbehandlerModel, PersonModel)> SaksbehandlereMedPerson { get; set; }
-
+    
     public IEnumerable<SelectListItem> ValgbareSaksbehandlere
     {
         get
--- ViewModels/KartfeilSkjemaViewModel.cs
+++ ViewModels/KartfeilSkjemaViewModel.cs
@@ -5,14 +5,12 @@ public class KartfeilSkjemaViewModel
     [Required(ErrorMessage = "Tittel må fylles ut")]
     [StringLength(100, ErrorMessage = "Tittel kan ikke være lengre enn 100 tegn")]
     public string Tittel { get; set; }
-
+    
     [Required(ErrorMessage = "Beskrivelse må fylles ut")]
     public string Beskrivelse { get; set; }
-
+    
     [Required]
     public string GeometriGeoJson { get; set; }
-
     public bool ErNodEtatKritisk { get; set; }
-
-    public string Prioritet { get; set; } // Settes basert på ErNodEtatKritisk
+    public string Prioritet { get; set; }
 }
