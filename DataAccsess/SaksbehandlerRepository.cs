b1bc95dccfe8d396c4eef5f7f5bcb965856b8452
Lene Olsen <leneo12@student.uia.no>
2024-11-25 12:27:43 +0100

    Fjernet ubrukte spørringer

--- DataAccsess/SaksbehandlerRepository.cs
+++ DataAccsess/SaksbehandlerRepository.cs
@@ -15,47 +15,6 @@ public class SaksbehandlerRepository : ISaksbehandlerRepository
         _dbConnection = dbConnection;
     }
 
-    public async Task<bool> ErGyldigSaksbehandlerEpost(string epost)
-    {
-        const string sql = @"
-            SELECT COUNT(1) 
-            FROM saksbehandler s
-            WHERE s.jobbepost = @Epost";
-
-        using var connection = _dbConnection.CreateConnection();
-        var count = await connection.QuerySingleOrDefaultAsync<int>(sql, new { Epost = epost });
-        return count > 0;
-    }
-
-    public async Task<SaksbehandlerModel?> HentSaksbehandlerMedEpost(string epost)
-    {
-        const string sql = @"
-            SELECT s.saksbehandler_id as SaksbehandlerId,
-                   s.person_id as PersonId,
-                   s.jobbepost as Jobbepost,
-                   s.jobbtelefon as Jobbtelefon,
-                   s.stilling as Stilling
-            FROM saksbehandler s
-            WHERE s.jobbepost = @Epost";
-
-        using var connection = _dbConnection.CreateConnection();
-        return await connection.QuerySingleOrDefaultAsync<SaksbehandlerModel>(sql, new { Epost = epost });
-    }
-
-    public async Task<SaksbehandlerModel?> HentSaksbehandler(int personId)
-    {
-        const string sql = @"
-            SELECT s.saksbehandler_id as SaksbehandlerId,
-                   s.person_id as PersonId,
-                   s.jobbepost as Jobbepost,
-                   s.jobbtelefon as Jobbtelefon,
-                   s.stilling as Stilling
-            FROM saksbehandler s
-            WHERE s.person_id = @PersonId";
-
-        using var connection = _dbConnection.CreateConnection();
-        return await connection.QuerySingleOrDefaultAsync<SaksbehandlerModel>(sql, new { PersonId = personId });
-    }
 
     public async Task<List<(SaksbehandlerModel, PersonModel)>> HentAlleSaksbehandlereMedPersonAsync()
     {
